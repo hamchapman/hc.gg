b540b29e692df0d637bd4124cf11f8184f1cea75
Hamilton Chapman <hamchapman@gmail.com>
2019-01-06 01:25:15 +0000

    Add CDN support in production

--- api/posts/index.js
+++ api/posts/index.js
@@ -2,4 +2,4 @@ const posts = require('../../posts.json');
 
 module.exports = (req, res) => {
   res.end(posts);
-}
+};
--- next.config.js
+++ next.config.js
@@ -1,1 +1,5 @@
-module.exports = {}
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isProd ? 'd3ts6yifqfl9ka.cloudfront.net' : ''
+};
