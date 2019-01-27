2c9dfa4ba3aac7b46fb5277cae2287d2f4091aa3
Hamilton Chapman <hamchapman@gmail.com>
2019-01-27 01:14:58 +0000

    Fix assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   target: 'serverless',
-  assetPrefix: isProd ? 'd3ts6yifqfl9ka.cloudfront.net' : ''
+  assetPrefix: isProd ? 'https://d3ts6yifqfl9ka.cloudfront.net' : ''
 };
